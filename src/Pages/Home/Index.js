a7fe291a694708e2f9107234d71f419f4b6ebdd5
NicolasG0ulart <nicolasgoulart5001@gmail.com>
2024-08-18 00:53:48 -0300

    resolve deploy

--- src/Pages/Home/Index.js
+++ src/Pages/Home/Index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Header from "../../Components/Header/Index"
 import Main from "./Main/Index";
 
-export default function(){
+export default  function Home(){
 
     return(
         <>
