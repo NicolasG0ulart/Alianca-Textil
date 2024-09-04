ef263f1b200540dba6441a0cb1512e857dff3f1b
Nicolas Goulart <135771923+NicolasG0ulart@users.noreply.github.com>
2024-09-04 07:13:02 -0300

    Update Contact.js
    add title in iframe

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -21,7 +21,7 @@ export default function Contact(){
                         <p>Professora Nair Temponi - n° NÃOSEIONÚMEROKK56sepa?</p>
                     </section>
                     <section>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4677.485739561293!2d-43.121789874783715!3d-21.8416619427358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x987d000b4eef4d%3A0x659eeba13559da94!2zQWxpYW7Dp2EgVMOqeHRpbA!5e1!3m2!1spt-PT!2sbr!4v1717946633097!5m2!1spt-PT!2sbr" ></iframe>
+                        <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4677.485739561293!2d-43.121789874783715!3d-21.8416619427358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x987d000b4eef4d%3A0x659eeba13559da94!2zQWxpYW7Dp2EgVMOqeHRpbA!5e1!3m2!1spt-PT!2sbr!4v1717946633097!5m2!1spt-PT!2sbr" ></iframe>
                     </section>
                 </S.Map>
                 <S.Contato>
